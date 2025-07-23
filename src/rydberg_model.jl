2411b3c83f41eb62976c0269923ae6c8aa5753d2
artem-gordeev <ar1emg0rdeev@gmail.com>
2025-07-24 00:52:57 +0300

    name mistake

--- src/rydberg_model.jl
+++ src/rydberg_model.jl
@@ -51,12 +51,12 @@ end;
 @inline function Ω(x, y, z, laser_params)
     if length(laser_params) == 5
         Ω0, w0, z0, θ, n = laser_params;
-        return Ω0 .* A_new(x, y, z, w0, z0; n=n, θ=θ) .* A_phase_new(x, y, z, w0, z0; θ=θ);
+        return Ω0 .* A(x, y, z, w0, z0; n=1, θ=0) .* A_phase(x, y, z, w0, z0; θ=0);
 
     elseif length(laser_params) == 6
         Ω0, w0, z0, beam_type, n, m = laser_params;
         if (n==0)&&(m==0)
-            return Ω0 .* A(x, y, z, w0, z0) .* A_phase(x, y, z, w0, z0);
+            return Ω0 .* A(x, y, z, w0, z0; n=1, θ=0) .* A_phase(x, y, z, w0, z0; θ=0);
         elseif beam_type == "simp_flattop_LG"
             return simple_flattopLG_field(x,y,z,laser_params)
         end;
@@ -64,7 +64,7 @@ end;
     elseif length(laser_params) == 7
         Ω0, w0, z0, beam_type, n, m, sqz = laser_params;
         if (n==0)&&(m==0)
-            return Ω0 .* A(x, y, z, w0, z0) .* A_phase(x, y, z, w0, z0)
+            return Ω0 .* A(x, y, z, w0, z0; n=1, θ=0) .* A_phase(x, y, z, w0, z0; θ=0)
         elseif beam_type == "simp_flattop_HG"
             return simple_flattopHG_field(x,y,z,laser_params)
         end;
